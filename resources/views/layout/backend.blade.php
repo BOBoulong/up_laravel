8508eb862bcbf6be74e28a6c337a21064a7ecfd6
rathanak <daromraksmeyrathanak@gmail.com>
2024-01-08 17:57:08 +0700

    remove policy and term condition

--- resources/views/layout/backend.blade.php
+++ resources/views/layout/backend.blade.php
@@ -83,11 +83,6 @@
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; Boulong Website 2024</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
                         </div>
                     </div>
                 </footer>
